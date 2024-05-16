bf00ce190b76e11126b45d447611467046277881
peinlcy <peinlcy@gmail.com>
2024-05-16 22:00:53 +0800

    partially impl verify pairing

--- src/pairing_verify.rs
+++ src/pairing_verify.rs
@@ -1,4 +1,7 @@
-use ark_bn254::{Fq, Fq12, G1Affine};
+use ark_bn254::{Fq, Fq12, Fr, G1Affine};
+use ark_ff::Field;
+
+use crate::utils::scalar_to_naf;
 
 // TODO
 pub struct Pairing;
@@ -7,11 +10,21 @@ impl Pairing {
     pub fn verify_pairings(
         eval_points: Vec<G1Affine>,
         lines: Vec<(Fq, Fq)>,
-        e: Fq,
+        e: Fr,
         c: Fq12,
         c_inv: Fq12,
         wi: Fq12,
     ) -> bool {
+        assert_eq!(eval_points.len(), lines.len());
+        assert_eq!(c * c_inv, Fq12::ONE);
+
+        // let mut f = Fq12::ONE;
+        let mut f = c_inv;
+        let mut naf_digits = scalar_to_naf(e);
+        naf_digits.reverse();
+        naf_digits.remove(0);
+        for (i, &digit) in naf_digits.iter().enumerate() {}
+
         true
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,21 @@
-pub fn to_naf(mut x: usize) -> Vec<usize> {
+use ark_bn254::Fr;
+
+pub fn to_naf(mut x: i64) -> Vec<i8> {
     let mut z = vec![];
     while x > 0 {
         if x % 2 == 0 {
             z.push(0);
         } else {
-            let zi = 2 - (x % 4);
-            x -= zi;
+            let zi: i8 = 2 - (x % 4) as i8;
+            x -= zi as i64;
             z.push(zi)
         }
 
         x = x / 2
     }
     return z;
+}
+
+pub fn scalar_to_naf(mut x: Fr) -> Vec<i8> {
+    vec![]
 }
