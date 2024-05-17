2ce7fd1f6d1c4f1c0f831ace7f6e5e5132cbca2e
peinlcy <peinlcy@gmail.com>
2024-05-17 16:30:37 +0800

    replace scalar from BigInt to BigUint

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use ark_bn254::{g1::G1Affine, Fq12};
+    use ark_bn254::Fq12;
     use ark_ec::{AffineRepr, CurveGroup};
-    use ark_ff::{BigInt, BigInteger, Field};
+    use ark_ff::{Field, One};
+    use num_bigint::BigUint;
+    use num_traits::FromPrimitive;
 
     use crate::{
         constant::{g1, g2},
@@ -11,11 +13,15 @@ mod tests {
 
     #[test]
     fn test_pairing_verify() {
-        let P1 = g1.mul_bigint(BigInt::<4>::new([3, 0, 0, 0])).into_affine();
-        let P2 = g1.mul_bigint(BigInt::<4>::one()).into_affine();
+        let P1 = g1
+            .mul_bigint(BigUint::from_i8(3).unwrap().to_u64_digits())
+            .into_affine();
+        let P2 = g1.mul_bigint(BigUint::one().to_u64_digits()).into_affine();
 
-        let Q1 = g2.mul_bigint(BigInt::<4>::one()).into_affine();
-        let Q2 = g2.mul_bigint(BigInt::<4>::new([3, 0, 0, 0])).into_affine();
+        let Q1 = g2.mul_bigint(BigUint::one().to_u64_digits()).into_affine();
+        let Q2 = g2
+            .mul_bigint(BigUint::from_i8(3).unwrap().to_u64_digits())
+            .into_affine();
 
         println!("Q1: {:?},\nQ2: {:?}", Q1, Q2);
 
