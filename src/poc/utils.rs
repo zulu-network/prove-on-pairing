e33e7eaba73f4bf6c558e274ce6c8887a4a9daab
Paul Cheng <chengyuejia@foxmail.com>
2024-05-23 16:39:02 +0800

    feat: test_mul_by_char

--- src/poc/utils.rs
+++ src/poc/utils.rs
@@ -65,7 +65,10 @@ mod test {
     use super::*;
     use crate::params;
 
-    use std::ops::Deref;
+    use ark_bn254::G2Affine;
+    use ark_ec::bn::g2::mul_by_char;
+    use ark_std::UniformRand;
+    use std::ops::{Deref, Neg};
 
     #[test]
     fn test_biguint_naf() {
@@ -99,4 +102,43 @@ mod test {
     //     };
     //     expect.add_in_place(&q);
     // }
+
+    #[test]
+    fn test_mul_by_char() {
+        let rng = &mut ark_std::test_rng();
+        let Q4 = G2Affine::rand(rng);
+
+        // ==== a. Compute phi_Q
+        let mut actual = mul_by_char::<ark_bn254::Config>(Q4.clone());
+
+        // 1. one-time frobenius map to compute phi_Q
+        // 2.1 Qx.conjugate * beta^{2 * (p - 1) / 6}
+        let mut Q4_x = Q4.x.clone();
+        Q4_x.conjugate_in_place();
+        Q4_x = Q4_x.mul(&params::BETA_PI_1[1]);
+
+        // 2.2 Qy.conjugate * beta^{3 * (p - 1) / 6}
+        let mut Q4_y = Q4.y.clone();
+        Q4_y.conjugate_in_place();
+        Q4_y = Q4_y.mul(&params::BETA_PI_1[2]);
+        let phi_Q = G2Affine::new(Q4_x, Q4_y);
+
+        assert_eq!(actual, phi_Q);
+
+        // ==== B. Compute phi_Q_2
+        let mut actual_2 = mul_by_char::<ark_bn254::Config>(phi_Q.clone());
+
+        // 2. one-time frobenius map to compute phi_Q
+        // 2.1  Qx * beta^{2 * (p^2 - 1) / 6}
+        let mut Q4_x = Q4.x.clone();
+        Q4_x = Q4_x.mul(&params::BETA_PI_2[1]);
+
+        // 2.2 -Qy
+        let Q4_y = Q4.y.clone().neg();
+
+        // 2.3(non-fixed) add line with T4 and phi(Q)
+        let phi_Q_2 = G2Affine::new(Q4_x, Q4_y);
+
+        assert_eq!(actual_2, phi_Q_2);
+    }
 }
